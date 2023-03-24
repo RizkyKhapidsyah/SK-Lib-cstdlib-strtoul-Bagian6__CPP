68b821bbd6dea227738a8aa542e7df2d3de446f4
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-03-24 21:35:25 +0700

    Initial commit

---
+++ SK-Lib-cstdlib-strtoul-Bagian6__CPP/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+
+/*
+    Source by Programiz (https://www.programiz.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+using namespace std;
+
+int main() {
+    char* end;
+
+    cout << "  25axbz" << " to Unsigned Long Long Int with base-11 = " << strtoull("  25axbz", &end, 11) << endl;
+    cout << "End String = " << end << endl << endl;
+
+    cout << "   -110bcd" << " to Unsigned Long Long Int with base-2 = " << strtoull("   -110bcd", &end, 2) << endl;
+    cout << "End String = " << end << endl << endl;
+
+    cout << "ax110.97" << " to Unsigned Long Long Int with base-10 = " << strtoull("ax110.97", &end, 10) << endl;
+    cout << "End String = " << end << endl << endl;
+
+    _getch();
+    return 0;
+}
